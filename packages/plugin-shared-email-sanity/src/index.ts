317f254d12c342c10e1a8cfca704d7db7fbcf338
caveJyn <k.ullah.93@gmail.com>
2025-08-11 17:50:02 -0400

    exported clearConnectionCache

--- packages/plugin-shared-email-sanity/src/index.ts
+++ packages/plugin-shared-email-sanity/src/index.ts
@@ -2,3 +2,4 @@ export * from "./emailTemplate";
 export * from "./types";
 export { isUserConnected } from "./isuserConnected"; // Export new user connection function
 export { resolveUserIdFromCreatedBy } from "./resolveUser";
+export { clearConnectionCache } from "./isuserConnected"; // Export cache clearing function
